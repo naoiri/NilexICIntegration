b77af220330f4e064bfa713a85b190d0c54e1a7e
naoiri <naoiri2001@skola.goteborg.se>
2022-03-10 10:09:22 +0100

    test through main changed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -25,14 +25,14 @@ public class App {
 			System.out.println(nd.postEntity(jo));
 		}*/
 		
-		NilexData nd = new NilexData("naoya.irikura@herrljunga.se", "Praktik2022");
+	
 		ICData icData = new ICData("naoyaTest", "naoyaTest");
 	
 		HttpResponse<String> guides = icData.getGuides();
 		
 		List<JSONObject> jsonObjects = icData.convertResponseToJson(guides);
 		for (JSONObject jo : jsonObjects) {
-			System.out.println(nd.postEntity(jo));
+			System.out.println(jo);
 		}
 		
 	}
